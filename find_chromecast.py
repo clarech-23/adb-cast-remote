391cae2f962a497348b85c7aef752dbd2d7f9957
clarech-23 <60282974+clarech-23@users.noreply.github.com>
2025-04-03 17:15:21 -0700

    Refactor adb connection functions into one single function

--- find_chromecast.py
+++ find_chromecast.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3  # TODO: What to do with this shebang?
 
-import chromecast_utils as utils
+import device_utils as utils
 
 if __name__ == '__main__':
     ip_address = utils.get_ip_address()
