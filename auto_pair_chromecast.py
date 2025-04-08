bbb962a6de9f929224fb7db086b82cf42a69483a
clarech-23 <60282974+clarech-23@users.noreply.github.com>
2025-04-07 19:34:59 -0700

    Change function name

--- auto_pair_chromecast.py
+++ auto_pair_chromecast.py
@@ -8,4 +8,4 @@ import device_utils as utils
 if __name__ == "__main__":
     utils.restart_adb_server()
     ip_address = utils.get_ip_address()
-    utils.attempt_to_connect(ip_address)
+    utils.attempt_auto_pair(ip_address)
